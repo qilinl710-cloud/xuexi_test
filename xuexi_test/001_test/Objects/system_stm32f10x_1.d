9341b65831b0a0af3ab5ac5a0e5515865a0e80fc
qilin zhiwang <725371704@qq.com>
2026-01-03 22:58:09 +0800

    添加忽略规则，过滤Keil垃圾文件

--- xuexi_test/001_test/Objects/system_stm32f10x_1.d
+++
@@ -1,5 +0,0 @@
-.\objects\system_stm32f10x_1.o: User\system_stm32f10x.c
-.\objects\system_stm32f10x_1.o: User\stm32f10x.h
-.\objects\system_stm32f10x_1.o: .\Start\core_cm3.h
-.\objects\system_stm32f10x_1.o: D:\arm_KeilMDK\ARM\arm5_compiler\Bin\..\include\stdint.h
-.\objects\system_stm32f10x_1.o: User\system_stm32f10x.h
--- xuexi_test/002_test/Objects/core_cm3.d
+++
@@ -1,2 +0,0 @@
-.\objects\core_cm3.o: Start\core_cm3.c
-.\objects\core_cm3.o: D:\arm_KeilMDK\ARM\arm5_compiler\Bin\..\include\stdint.h
